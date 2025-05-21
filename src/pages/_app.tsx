0ef04fc07d26fcbfab498b181335b46eca159baa
JayNaruto <jaynarutomistry@gmail.com>
2025-05-21 14:38:24 -0700

    Added speed insights from vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
+      <SpeedInsights/>
     </QueryClientProvider>
   )
 }
